987a351a7413c235b430f95e2ea52c454764c224
Bryan Joseph <bj97301@gmail.com>
2018-09-06 09:47:15 -0700

    look its broke as fuck

---
+++ newrelic.js
@@ -0,0 +1,28 @@
+const envName = process.env.NODE_ENV
+export const config = {
+    app_name: [`${envName}.mobileApi`],
+    license_key: 'b872f5b10d584b306e866adf8c0e808e8ba204d5',
+    logging: {
+        /**
+         * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+         * issues with the agent, 'info' and higher will impose the least overhead on
+         * production applications.
+         */
+        level: 'trace'
+    },
+    allow_all_headers: true,
+    attributes: {
+        exclude: [
+            'request.headers.cookie',
+            'request.headers.authorization',
+            'request.headers.proxyAuthorization',
+            'request.headers.setCookie*',
+            'request.headers.x*',
+            'response.headers.cookie',
+            'response.headers.authorization',
+            'response.headers.proxyAuthorization',
+            'response.headers.setCookie*',
+            'response.headers.x*'
+        ]
+    }
+}
